6e57665faccff4ba8624ccc1bb9c7f1d8575c823
MasPort <maspot42019@gmail.com>
2026-02-10 23:04:29 -0700

    casos de ejemplo

--- main.cpp
+++ main.cpp
@@ -6,5 +6,69 @@ using namespace std;
 
 int main()
 {
+    /*Matriz A;
+    Matriz B(2, 2);
+
+    B = A;
+    cout << B << endl;*/
+
+    /*Matriz A;
+    Matriz B;
+
+    cin >> A;
+    cout << A << endl;
+
+    cin >> B;
+    cout << B << endl;
+
+    cout << A + B << endl;
+    cout << A - B << endl;*/
+
+    /*Matriz A;
+    Matriz B;
+
+    cin >> A;
+    cout << A << endl;
+
+    cin >> B;
+    cout << B << endl;
+
+    cout << A * B << endl;
+    cout << B * A << endl;*/
+
+    /*Matriz A;
+
+    cin >> A;
+    cout << A << endl;
+
+    cout << 5 * A << endl;
+    cout << A * 5 << endl;*/
+
+    /*Matriz A;
+    cin >> A;
+    cout << A << endl;
+
+    cout << A[0][0] << endl;*/
+
+    /*Matriz A;
+    cin >> A;
+    cout << A << endl;
+    A.transpuesta();
+
+    cout << A << endl;*/
+
+    /*Matriz A;
+    cin >> A;
+    cout << A << endl;
+
+    cout << A.inversa() << endl;*/
+
+    /*Matriz A;
+    cin >> A;
+    cout << A << endl;
+    A.redimensionar(5, 4);
+
+    cout << A << endl;*/
+
     return 0;
 }
